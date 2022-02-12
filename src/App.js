67ef69446c450cb010dbdaafed143069e53d7159
Dylan King <dwkblue51@yahoo.com>
2022-02-12 17:27:58 -0600

    initialize firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import getFirebaseConfig from './firebase-config';
 import {initializeApp} from 'firebase/app';
 import './App.css';
+import Home from './components/Home';
 
 const firebaseAppConfig = getFirebaseConfig();
 initializeApp(firebaseAppConfig);
 
 function App() {
   return (
- //stuff
+    <main>
+      <header>
+        <h1>Where's Waldo</h1>
+      </header>
+      <Home />
+      <footer>
+        <p>Created by Dylan King</p>
+      </footer>
+    </main>
   );
 }
 
