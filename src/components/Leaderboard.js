cc48591d487d6631b067959704aa3bca8aa10085
Dylan King <dwkblue51@yahoo.com>
2022-03-02 13:02:52 -0600

    map over leaderboard state

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Card from "./Card";
 
-const Leaderboard = ({Leaderboard}) => {
+const Leaderboard = ({highScores}) => {
+    const cards = highScores.map((item) => {
+        return <Card name={item.data.name} time={item.data.time} key={item.id} />
+    })
     return (
         <div>
-
+            {cards}
         </div>
     )
 }
