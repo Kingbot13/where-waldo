eb22a646518e49cf87003e37360246613b118832
Dylan King <dwkblue51@yahoo.com>
2022-03-07 15:45:37 -0600

    map over characterLocation to create area elements, and style the main game image

--- src/components/GameLevel.js
+++ src/components/GameLevel.js
@@ -36,14 +36,22 @@ const StartButton = styled.button`
     transition: height 0.5s, width 0.5s, font-size 0.5s;
 `
 
-const GameLevel = ({image, imageClick, position, show, storeLocation, validate, correctSelections}) => {
+const MainImg = styled.img`
+    width: 100vw;
+    height: auto;
+`
+
+const GameLevel = ({image, imageClick, position, show, storeLocation, validate, correctSelections, characterLocation}) => {
     const [showButton, setShowButton] = React.useState(true);
     const [seconds, setSeconds] = React.useState(0);
     const [showScore, setShowScore] = React.useState(false);
     const [leaderboard, setLeaderboard] = React.useState([]);
     const [value, setValue] = React.useState('');
     const [showLeaderboard, setShowLeaderboard] = React.useState(false);
-    // const [highScore, setHighScore] = React.useState({});
+
+    const areas = characterLocation.map((item) => {
+        return <area key={item.name} onClick={(e) => storeLocation(e)} coords={item.coords} shape='rect' alt={item.name} />
+    });
 
     const buttonClick = () => {
         setShowButton(!showButton);
@@ -75,6 +83,7 @@ const GameLevel = ({image, imageClick, position, show, storeLocation, validate, 
         e.preventDefault();
         addHighScore();
         setShowLeaderboard(true);
+        e.target.removeEventListener('click', submitScore);
     }
 
     const addHighScore = async () => {
@@ -101,12 +110,13 @@ const GameLevel = ({image, imageClick, position, show, storeLocation, validate, 
 
     return (
         <StyledDiv onClick={(e)=> imageClick(e)} >
-            <img src={image} alt='' useMap="#waldo-map" />
+            <MainImg src={image} alt='' useMap="#waldo-map" />
             <map name="waldo-map">
-                <area onClick={(e)=>storeLocation(e)} shape="rect" coords="546,832,590,864" alt="wilma" />
+                {/* <area onClick={(e)=>storeLocation(e)} shape="rect" coords="546,832,590,864" alt="wilma" />
                 <area onClick={(e)=>storeLocation(e)} shape="rect" coords="1892,48,1947,112" alt="waldo" />
                 <area onClick={(e)=>storeLocation(e)} shape="rect" coords="1819,710,1865,793" alt="odlaw" />
-                <area onClick={(e)=>storeLocation(e)} shape="rect" coords="552,466,635,596" alt="wizard" />
+                <area onClick={(e)=>storeLocation(e)} shape="rect" coords="552,466,635,596" alt="wizard" /> */}
+                {areas}
             </map>
             {show && <DropMenu top={position.top} left={position.left} validate={validate} />}
             {showButton && <StartButton onClick={buttonClick}>Start</StartButton>}
